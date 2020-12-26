7738446447725b46f72de1e8d79ed341c2bd28ba
MichalKwarta <62547595+MichalKwarta@users.noreply.github.com>
2020-12-26 15:51:09 +0100

    Update findparams.py

--- findparams.py
+++ findparams.py
@@ -3,14 +3,12 @@ from multiprocessing import Pool,freeze_support
 def main():
     l=Point.read("berlin52.txt")
     solutions=[]
-    total=1
     for eva in range(1,11): #11
         for alpha in range(1,100,5):
             for beta in range(1,100,5):
                 for count in range(10,501,50):
                         args=[(l,eva/10,alpha,beta,count,it) for it in range(5,100,5)]
                         print(args[1:])
-                        total+=3
                         print(eva/10,alpha,beta,count)
                         with Pool(3) as pool:
                             results=pool.starmap(Point.Ants,args)
