6116cf330f69ab5e61e4934557d296fc5424e4fa
Michał Kwarta <Michu1164@gmail.com>
2022-03-17 19:54:48 +0100

    refactor: :fire: code refractor;migrate to networkx;create venv
    code cleanup with removal of unecessary functions, added new class - PointsList as aggregator of Points,replaced matplotlib with networkx,removed testing files,translated polish comments

--- main.py
+++ main.py
@@ -1,53 +1,10 @@
-from point import *
-from drawing import draw
-from os import listdir,getcwd
-from time import time,localtime,strftime
-
-eva,alpha,beta,ants=0.2, 15, 200,30
-eva,alpha,beta,ants=0.15,9,12,30
 
 
+from point import  PointsList
 
-eva,alpha,beta,ants=0.2, 15, 200,30
-eva,alpha,beta,ants= 0.2,6,61,30
-
-start=time()
-
-files=[f[:-4] for f in listdir(getcwd()+"/data")]
+FILENAME = "data/berlin52.txt"
 
 
-distances_Ants={instance:
-Point.Ants(
-l:=Point.read(getcwd()+"\\data\\"+instance+'.txt'),eva,alpha,beta,30)
-for instance in 
-files}
+points = PointsList(FILENAME)
 
-ACO_TIME=time()-start
-start=time()
-
-distances_Greedy = {instance:Point.greedy(Point.readPoints(getcwd()+"\\data\\"+instance+'.txt'))[-1] for instance in files}
-
-GREEDY_TIME=time()-start
-print("\anazwa instancji\twynik\tczas testu: ",pomiar:=strftime("%H:%M",localtime()))
-print("ACO")
-#berlin działa super dla eva,alpha,beta,ants=0.2,5,3,30
-#wstepnie optymalne parametry = 0.2,6,61,30
-for name,dist in distances_Ants.items():
-    print(f"{name}\t{str(round(dist[1],2)).replace('.',',')}")
-print("GREEDY")
-for name,dist in distances_Greedy.items():
-    print(f"{name}\t{str(round(dist,2)).replace('.',',')}")
-print(f"ACO liczyło {round(ACO_TIME)}s\t GREEDY liczył {round(GREEDY_TIME)}s")
-
-paramslist=[eva,alpha,beta,ants]
-fname=str(paramslist)[1:-1].replace(',','_')+'_'+pomiar.replace(':','_')+'.txt'
-
-with open(fname,'w') as f:
-    for name,comb in distances_Ants.items():
-        print(name,comb[0],file=f)
-    for name,comb in distances_Ants.items():
-        print(name,comb[1],file=f)
-
-
-
-    
+path, _ = points.Ants(plot=True)
--- point.py
+++ point.py
@@ -1,101 +1,120 @@
-from random import randint, random
-from time import time
+import itertools
+import random
+import time
+import networkx as nx
+from typing import Tuple
+import matplotlib.pyplot as plt
+
 MAX = 600
-TIME_LIMIT=200
+ITERATION_LIMIT = 30
 
-class Point:
-    count = 0
-    cords = []  # check if coordinates were already used
 
-    def __init__(self, id, X, Y):
-        Point.count += 1
-        self.id = id
-        if (X, Y) in Point.cords:
-            raise("Już jest taki punkt")
-        Point.cords.append((X, Y))
-        self.x = X
-        self.y = Y
+class PointsList:
+    points_list:list["Point"]
 
-    def __del__(self):
-        Point.count -= 1
-        Point.cords.remove((self.x, self.y))
+    def __init__(self, filename: str):
+        if filename is not None:
+            self.readPoints(filename)
 
-    @classmethod
-    def random(cls):
-        X, Y = randint(0, MAX), randint(0, MAX)
-        while (X, Y) in Point.cords:
-            X, Y = randint(0, MAX), randint(0, MAX)
-        return cls(Point.count+1, X, Y)
+    def __repr__(self):
+        return list(map(str,self.points_list))
 
-    def getdistance(self, p2):
-        return ((self.x-p2.x)**2+(self.y-p2.y)**2)**0.5
+    def readPoints(self,filename):
+        with open(filename, "r") as f:
+            count = int(f.readline())
+            l = f.readlines()
+            if len(l) != count:
+                raise ("Error in data")
+            obj_list = []
+            for el in l:
+                a, b, c = map(int, el[:-1].split())
+                obj_list.append(Point(a-1, b, c))
 
-    def getclosest(self, l: list):  # zakładam, że nie znajduje sie na liście
-        closest = l[0]
-        delta_distance = self.getdistance(closest)
-        for point in l[1::]:
-            if self.getdistance(point) < self.getdistance(closest):
-                closest = point
-                delta_distance = self.getdistance(closest)
-        return closest, delta_distance
+        self.points_list = obj_list
 
-    def __str__(self):
-        return f"{self.id} {self.x} {self.y}"
+    def randomPoints(self, n):
+        self.points_list = Point.randomPointsList(n)
 
-    @staticmethod
-    def Ants(points_list,evaporation_rate = 0.2,
-        alpha = 6,
-        beta = 61,
-        ants_count = 30,
-        ):
+    def greedy(self,plot=False):
+        path = [self.points_list[0]]  # first node as start
+        self.points_list = self.points_list[1::]
+        distance = 0
+
+        while self.points_list != []:
+            next, delta = path[-1].getclosest(self.points_list)
+            distance += delta
+            path.append(next)
+            self.points_list.pop(self.points_list.index(next))
+
+        distance += path[0].getdistance(path[-1])  # go back to first node
+        path.append(path[0])
+        path = [point.id for point in path]
+        if plot:
+            self.plot(path)
+            
+        return path, distance
+
+    def Ants(
+        self,
+        plot=False,
+        iteration_limit: int = ITERATION_LIMIT,
+        evaporation_rate: float = 0.2,
+        alpha: int = 6,
+        beta: int = 61,
+        ants_count: int = 30,
+    ):
         # Theory -> https://www.youtube.com/watch?v=783ZtAF4j5g
-        distance_matrix = []
-        pheromones_matrix = []
-        
+        distance_matrix: list[float] = []
+        pheromones_matrix: list[float] = []
 
-        for p1 in points_list:
+        for p1 in self.points_list:
             costs_row = []
             pheromones_row = []
-            for p2 in points_list:
+            for p2 in self.points_list:
                 if p1 == p2:
                     costs_row.append(0)
                     pheromones_row.append(0)
                 else:
-                    dist = ((p1[1]-p2[1])**2+(p1[2]-p2[2])**2)**0.5
+                    dist = p1.getdistance(p2)
                     costs_row.append(dist)
                     try:
-                        pheromones_row.append(1/dist)
-                    except ZeroDivisionError:
-                        print(p1,p2)
+                        pheromones_row.append(1 / dist)
+                    except ZeroDivisionError:  # well, it shouldn't be possible
+                        print(p1, p2)
                         break
             distance_matrix.append(costs_row)
             pheromones_matrix.append(pheromones_row)
         res = []
-        start=time()
-        while((time()-start)<TIME_LIMIT):  #TODO: Mieli 50 sekund
-            #print(i, end=" ")
+        start = time.time()
+        iteration = 0
+        while iteration < iteration_limit:  # TODO: Mieli 50 sekund
+            iteration += 1
+            # print(i, end=" ")
             resant = []
-            for _ in range(ants_count):  
-                current = randint(0,len(points_list)-1)
+            for _ in range(ants_count):
+                current = random.randint(0, len(self.points_list) - 1)
                 visited = [0 for _ in range(len(distance_matrix))]
                 path = [current]
                 dist = 0
-                while(len(path) < len(distance_matrix)):
+                while len(path) < len(distance_matrix):
                     visited[current] = 1
                     numerators = []
                     denominator = 0
-                    for ph, d, v in zip(pheromones_matrix[current], distance_matrix[current], visited):
-                        if v==1 or d == 0:
+                    for ph, d, v in zip(
+                        pheromones_matrix[current], distance_matrix[current], visited
+                    ):
+                        if v == 1 or d == 0:
                             val = 0
                         else:  # not visited and not (x,x) pair
-                            val = ph**alpha+(1/d)**beta
+                            val = ph**alpha + (1 / d) ** beta
                         numerators.append(val)
                         denominator += val
                     probabilities = list(
-                        map(lambda numerator: numerator / denominator, numerators))
+                        map(lambda numerator: numerator / denominator, numerators)
+                    )
 
                     # roulette wheel
-                    choice_num = random()  # <0,1)
+                    choice_num = random.random()  # <0,1)
                     probabilities_sum = 0
                     for V, P in enumerate(probabilities):
                         probabilities_sum += P
@@ -111,181 +130,89 @@ class Point:
             # update pheromones
             for i in range(len(distance_matrix)):  # evaporation
                 for j in range(len(distance_matrix)):
-                    pheromones_matrix[i][j] *= (1-evaporation_rate)
-
+                    pheromones_matrix[i][j] *= 1 - evaporation_rate
 
             for path, length in res:  # add pheromones after iteration
-                #local ph update
-                for i in range(len(path)-1):
+                # local ph update
+                for i in range(len(path) - 1):
                     v1 = path[i]
-                    v2 = path[i+1]
-                    pheromones_matrix[v1][v2] += 1/length
-                    pheromones_matrix[v2][v1] += 1/length
-            
-            
-            res+=resant
-            
-        #print()
+                    v2 = path[i + 1]
+                    pheromones_matrix[v1][v2] += 1 / length
+                    pheromones_matrix[v2][v1] += 1 / length
+
+            res += resant
+
+        # print()
         best = min(res, key=lambda x: x[1])
-        return list(map(lambda x: x + 1, best[0])), best[1]
+        if plot:
+            self.plot(best[0])
 
-    @staticmethod
-    def greedy(listofinstances):
-        path = [listofinstances[0]]  # biore pierwszy wierzcholek jako poczatek
-        listofinstances = listofinstances[1::]
-        distance = 0
+        return best
+    def plot(self,path):
+        G = nx.DiGraph(directed=True)
+        for p in self.points_list:
+            G.add_node(p.id,pos=(p.x,p.y))
 
-        while listofinstances != []:
-            next, delta = path[-1].getclosest(listofinstances)
-            distance += delta
-            path.append(next)
-            listofinstances.pop(listofinstances.index(next))
-
-        distance += path[0].getdistance(path[-1])  # zamykam cykl
-        path.append(path[0])
-        return path, distance
-
-    @staticmethod
-    def write(filename, list, distance=None):
-        f = open(filename, 'w+')
-        print(Point.count, file=f)
-        for object in list:
-            print(object, file=f)
-        print(distance, file=f)
-
-    @staticmethod
-    def read(filename):
-        f = open(filename, 'r')
-        count = int(f.readline())
-        l = f.readlines()
-        if len(l) != count:
-            print(filename)
-            raise('błąd danych')
-        obj_list = []
-        for el in l:
-            a, b, c = map(int, el[:-1].split())
-            obj_list.append((a, b, c))
-        f.close()
-        return obj_list
-    @staticmethod
-    def readPoints(filename):
-        f = open(filename, 'r')
-        count = int(f.readline())
-        l = f.readlines()
-        if len(l) != count:
-            raise('błąd danych')
-        obj_list = []
-        for el in l:
-            a, b, c = map(int, el[:-1].split())
-            obj_list.append(Point(a, b, c))
-        f.close()
-        return obj_list
-
-    @staticmethod
-    def Ants_testing(points_list,evaporation_rate = 0.1,
-        alpha = 5,
-        beta = 1,
-        ants_count = 30,
-        iterations=0
-        ):
-        # Theory -> https://www.youtube.com/watch?v=783ZtAF4j5g
-        distance_matrix = []
-        pheromones_matrix = []
-        partial_results=[]
-
-        for p1 in points_list:
-            costs_row = []
-            pheromones_row = []
-            for p2 in points_list:
-                if p1 == p2:
-                    costs_row.append(0)
-                    pheromones_row.append(0)
-                else:
-                    dist = ((p1[1]-p2[1])**2+(p1[2]-p2[2])**2)**0.5
-                    costs_row.append(dist)
-                    pheromones_row.append(1/dist)
-            distance_matrix.append(costs_row)
-            pheromones_matrix.append(pheromones_row)
-        res = []
-        
-        while(iterations<20):   #20 iteracji na sztywno
-            iterations+=1
-            #print(i, end=" ")
-            resant = []
-            for _ in range(ants_count):  
-                current = randint(0,len(points_list)-1)
-                visited = [0 for _ in range(len(distance_matrix))]
-                path = [current]
-                dist = 0
-                while(len(path) < len(distance_matrix)):
-                    visited[current] = 1
-                    numerators = []
-                    denominator = 0
-                    for ph, d, v in zip(pheromones_matrix[current], distance_matrix[current], visited):
-                        if v==1 or d == 0:
-                            val = 0
-                        else:  # not visited and not (x,x) pair
-                            val = ph**alpha+(1/d)**beta
-                        numerators.append(val)
-                        denominator += val
-                    probabilities = list(
-                        map(lambda numerator: numerator / denominator, numerators))
-
-                    # roulette wheel
-                    choice_num = random()  # <0,1)
-                    probabilities_sum = 0
-                    for V, P in enumerate(probabilities):
-                        probabilities_sum += P
-                        if choice_num <= probabilities_sum and P != 0:
-                            dist += distance_matrix[current][V]
-                            path.append(V)
-                            current = V
-                            break
-                path.append(path[0])
-                dist += distance_matrix[current][path[-1]]
-                resant.append([path, dist])
-
-            # update pheromones
-            for i in range(len(distance_matrix)):  # evaporation
-                for j in range(len(distance_matrix)):
-                    pheromones_matrix[i][j] *= (1-evaporation_rate)
+        for id, p in enumerate(path[:-1]):
+            G.add_edges_from(
+                [(p, path[id + 1])], weight=round(self.points_list[p].getdistance(self.points_list[path[id + 1]]),2),color='r'
+            )
+        edge_labels = dict(
+            [
+                (
+                    (
+                        u,
+                        v,
+                    ),
+                    d["weight"],
+                )
+                for u, v, d in G.edges(data=True)
+            ]
+        )
+        pos=nx.get_node_attributes(G,'pos')
+        _,colors = zip(*nx.get_edge_attributes(G,'color').items())
+        nx.draw(G,pos,with_labels=True,edge_color=colors,node_color='#93ccfa',node_size=500)
+        labels = nx.get_edge_attributes(G,'weight')
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+        plt.show()
 
 
-            for path, length in resant:  # add pheromones after iteration
-                #local ph update
-                for i in range(len(path)-1):
-                    v1 = path[i]
-                    v2 = path[i+1]
-                    pheromones_matrix[v1][v2] += 1/length
-                    pheromones_matrix[v2][v1] += 1/length
+class Point:
+    cords: list[Tuple[int, int]] = []  # check if coordinates were already used
 
-            res+=resant
-            tmp=min(resant,key=lambda x:x[1])
-            tmp[0]=list(map(lambda x:x+1,tmp[0]))
-            partial_results+=[tmp]
-            
-        
-        return partial_results
+    def __init__(self, id: int, X: int, Y: int):
 
+        if (X, Y) in Point.cords:
+            raise (f"Point {X} {Y} already exists")
 
-def generatelistofinstances(n: int):
-    l = []
-    for k in range(n):
-        l.append(Point.random())
-    return l
+        Point.cords.append((X, Y))
+        self.x: int = X
+        self.y: int = Y
+        self.id = id
+        id += 1
 
+    def __del__(self):
+        Point.cords.remove((self.x, self.y))
 
-def generatematrix(l: list):
-    matrix = []
-    for p1 in l:
-        row = []
-        for p2 in l:
-            if p2 == p1:
-                row.append(-1)
-            else:
-                # FIXME: TO TYLKO POGLĄDOWE BO JEST ZAOKRĄGLANIE
-                row.append(round(p1.getdistance(p2)))
-        matrix.append(row)
-    return matrix
+    @classmethod
+    def randomPointsList(cls, n: int):
+        possibleCords = list(itertools.product(range(MAX), range(MAX)))
+        return [cls(X, Y) for (X, Y) in random.sample(possibleCords, n)]
+
+    def getdistance(self, p2: "Point"):
+        return ((self.x - p2.x) ** 2 + (self.y - p2.y) ** 2) ** 0.5
+
+    def getclosest(self, l: list["Point"]):  # assumption that node is not in the list
+        closest: "Point" = l[0]
+        for point in l[1:]:
+            if self.getdistance(point) < self.getdistance(closest):
+                closest = point
+
+        delta_distance: float = self.getdistance(closest)
+
+        return closest, delta_distance
+
+    def __repr__(self):
+        return f"{self.id} {self.x} {self.y}"
 
 
