daebd65eac8d10217c0c61a7c534a5a29bcb490c
Michał Kwarta <Michu1164@gmail.com>
2022-03-17 21:43:11 +0100

    fix: :art: bugfix;add docs;refractor
    bugifx(bug created with auto renaming) ; refractor of names;create full documentation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-
-
-from point import  PointsList
+from point import  NodesList
 
 FILENAME = "data/berlin52.txt"
 
 
-points = PointsList(FILENAME)
-
-path, _ = points.Ants(plot=True)
+# points = PointsList(FILENAME)
+# path, _ = points.Ants(plot=True)
+nodes = NodesList()
+nodes.readNodes("data/test5.txt")
+path, distance = nodes.Ants(plot=True)
+print(distance)
+path,distance = nodes.greedy(plot=True)
+print(distance)
--- point.py
+++ point.py
@@ -1,57 +1,82 @@
 import itertools
 import random
-import time
-import networkx as nx
 from typing import Tuple
+
 import matplotlib.pyplot as plt
+import networkx as nx
 
 MAX = 600
 ITERATION_LIMIT = 30
 
 
-class PointsList:
-    points_list:list["Point"]
+class NodesList:
 
-    def __init__(self, filename: str):
+    nodes_list: list["Node"]
+
+    def __init__(self, filename: str = None):
         if filename is not None:
-            self.readPoints(filename)
+            self.readNodes(filename)
 
     def __repr__(self):
-        return list(map(str,self.points_list))
+        return list(map(str, self.nodes_list))
 
-    def readPoints(self,filename):
+    def readNodes(self, filename: str):
+        """
+        Read data from file,generate nodes and assign it to self.nodes_list
+
+        Args:
+            filename (str): path to datafile
+        Raises:
+            Exception: length of Nodes unequal to number declared in data file
+
+        """
+
         with open(filename, "r") as f:
             count = int(f.readline())
             l = f.readlines()
             if len(l) != count:
-                raise ("Error in data")
+                raise Exception("Error in data")
             obj_list = []
             for el in l:
                 a, b, c = map(int, el[:-1].split())
-                obj_list.append(Point(a-1, b, c))
+                obj_list.append(Node(a - 1, b, c))
+        self.nodes_list = obj_list
 
-        self.points_list = obj_list
+    def setRandomData(self, n: int):
+        """
+        Sets self.nodes_list to list of randomly genrated nodes
 
-    def randomPoints(self, n):
-        self.points_list = Point.randomPointsList(n)
+        Args:
+            n (int): Number of nodes to generate
+        """
+        self.nodes_list = Node.randomNodesList(n)
 
-    def greedy(self,plot=False):
-        path = [self.points_list[0]]  # first node as start
-        self.points_list = self.points_list[1::]
+    def greedy(self, plot=False) -> Tuple[list[int], float]:
+        """
+        Greedy approach to TSP problem
+
+        Args:
+            plot (bool, optional): if set to true, shows plotted path with edges weights. Defaults to False.
+
+        Returns:
+            Tuple[list[int],float]: Tuple with list of nodes on path and total distance
+        """
+        path = [self.nodes_list[0]]  # first node as start
+        nodesToVisit = self.nodes_list[1:]
         distance = 0
-
-        while self.points_list != []:
-            next, delta = path[-1].getclosest(self.points_list)
+        while nodesToVisit != []:
+            next, delta = path[-1].getclosest(nodesToVisit)
             distance += delta
             path.append(next)
-            self.points_list.pop(self.points_list.index(next))
+            nodesToVisit.pop(nodesToVisit.index(next))
 
         distance += path[0].getdistance(path[-1])  # go back to first node
         path.append(path[0])
-        path = [point.id for point in path]
+        path = [node.id for node in path]
+
         if plot:
             self.plot(path)
-            
+
         return path, distance
 
     def Ants(
@@ -62,15 +87,30 @@ class PointsList:
         alpha: int = 6,
         beta: int = 61,
         ants_count: int = 30,
-    ):
+    ) -> Tuple[list[int], float]:
+        """
+        Ant colony optimization algorithm to solve TSP problem
+
+        Args:
+            plot (bool, optional): if set to true,plots path. Defaults to False.
+            iteration_limit (int, optional): number of iterations for algorithm. Defaults to ITERATION_LIMIT.
+            evaporation_rate (float, optional): pheromone loss over iterations. Defaults to 0.2.
+            alpha (int, optional): weight of pheromones for heuristics. Defaults to 6.
+            beta (int, optional): weight of distance for heuristics. Defaults to 61.
+            ants_count (int, optional): number of ants in single iterations. Defaults to 30.
+
+        Returns:
+            Tuple[list[int],float]: Tuple with list of nodes on path and total distance
+
+        """
         # Theory -> https://www.youtube.com/watch?v=783ZtAF4j5g
         distance_matrix: list[float] = []
         pheromones_matrix: list[float] = []
 
-        for p1 in self.points_list:
+        for p1 in self.nodes_list:
             costs_row = []
             pheromones_row = []
-            for p2 in self.points_list:
+            for p2 in self.nodes_list:
                 if p1 == p2:
                     costs_row.append(0)
                     pheromones_row.append(0)
@@ -85,14 +125,12 @@ class PointsList:
             distance_matrix.append(costs_row)
             pheromones_matrix.append(pheromones_row)
         res = []
-        start = time.time()
         iteration = 0
-        while iteration < iteration_limit:  # TODO: Mieli 50 sekund
+        while iteration < iteration_limit:
             iteration += 1
-            # print(i, end=" ")
             resant = []
             for _ in range(ants_count):
-                current = random.randint(0, len(self.points_list) - 1)
+                current = random.randint(0, len(self.nodes_list) - 1)
                 visited = [0 for _ in range(len(distance_matrix))]
                 path = [current]
                 dist = 0
@@ -142,71 +180,101 @@ class PointsList:
 
             res += resant
 
-        # print()
         best = min(res, key=lambda x: x[1])
         if plot:
             self.plot(best[0])
 
         return best
-    def plot(self,path):
+
+    def plot(self, path: list[int]):
+        """
+        Method that plots path
+
+        Args:
+            path (list[int]): list of nodes on path
+        """
+
         G = nx.DiGraph(directed=True)
-        for p in self.points_list:
-            G.add_node(p.id,pos=(p.x,p.y))
+        for p in self.nodes_list:
+            G.add_node(p.id, pos=(p.x, p.y))
 
         for id, p in enumerate(path[:-1]):
             G.add_edges_from(
-                [(p, path[id + 1])], weight=round(self.points_list[p].getdistance(self.points_list[path[id + 1]]),2),color='r'
+                [(p, path[id + 1])],
+                weight=round(
+                    self.nodes_list[p].getdistance(self.nodes_list[path[id + 1]]), 2
+                ),
+                color="r",
             )
-        edge_labels = dict(
-            [
-                (
-                    (
-                        u,
-                        v,
-                    ),
-                    d["weight"],
-                )
-                for u, v, d in G.edges(data=True)
-            ]
+
+        pos = nx.get_node_attributes(G, "pos")
+        _, colors = zip(*nx.get_edge_attributes(G, "color").items())
+        nx.draw(
+            G,
+            pos,
+            with_labels=True,
+            edge_color=colors,
+            node_color="#93ccfa",
+            node_size=500,
         )
-        pos=nx.get_node_attributes(G,'pos')
-        _,colors = zip(*nx.get_edge_attributes(G,'color').items())
-        nx.draw(G,pos,with_labels=True,edge_color=colors,node_color='#93ccfa',node_size=500)
-        labels = nx.get_edge_attributes(G,'weight')
-        nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+        labels = nx.get_edge_attributes(G, "weight")
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
         plt.show()
 
 
-class Point:
+class Node:
     cords: list[Tuple[int, int]] = []  # check if coordinates were already used
-
     def __init__(self, id: int, X: int, Y: int):
 
-        if (X, Y) in Point.cords:
-            raise (f"Point {X} {Y} already exists")
+        if (X, Y) in Node.cords:
+            raise (f"Node {X} {Y} already exists")
 
-        Point.cords.append((X, Y))
+        Node.cords.append((X, Y))
         self.x: int = X
         self.y: int = Y
         self.id = id
-        id += 1
 
     def __del__(self):
-        Point.cords.remove((self.x, self.y))
+        Node.cords.remove((self.x, self.y))
 
     @classmethod
-    def randomPointsList(cls, n: int):
-        possibleCords = list(itertools.product(range(MAX), range(MAX)))
-        return [cls(X, Y) for (X, Y) in random.sample(possibleCords, n)]
+    def randomNodesList(cls, n: int) -> list["Node"]:
+        """Classmethod, generates list of random Nodes with cords in range <0;MAX>
+        MAX is defined in class,default 600
 
-    def getdistance(self, p2: "Point"):
+        Returns:
+            list[Node]: List of Nodes
+        """
+        possibleCords = list(itertools.product(range(MAX), range(MAX)))
+
+        return [
+            cls(id, X, Y) for (id, (X, Y)) in enumerate(random.sample(possibleCords, n))
+        ]
+
+    def getdistance(self, p2: "Node") -> float:
+        """calculate distance betweend two nodes
+
+        Args:
+            p2 (Node): Another Node to get distance to
+
+        Returns:
+            float: distance
+        """
         return ((self.x - p2.x) ** 2 + (self.y - p2.y) ** 2) ** 0.5
 
-    def getclosest(self, l: list["Point"]):  # assumption that node is not in the list
-        closest: "Point" = l[0]
-        for point in l[1:]:
-            if self.getdistance(point) < self.getdistance(closest):
-                closest = point
+    def getclosest(self, l: list["Node"]) -> Tuple["Node", float]:
+        """
+        Method to find closest Node and calculate distance to it
+
+        Args:
+            l(list["Node"]): List of remaining Nodes to visit
+        Returns:
+            Tuple["Node",float]: Closest Node and distance to it
+        """
+        closest: "Node" = l[0]
+        for node in l[1:]:
+            if self.getdistance(node) < self.getdistance(closest):
+                closest = node
 
         delta_distance: float = self.getdistance(closest)
 
@@ -214,5 +282,3 @@ class Point:
 
     def __repr__(self):
         return f"{self.id} {self.x} {self.y}"
-
-
